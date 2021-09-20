88859a458ca3b54e7d69c99c679fc2130f3a3791
jesus-molano <jessumolano@gmail.com>
2021-09-20 18:37:10 +0100

    Adding first hooks

---
+++ useFetch.js
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react"
+
+const useFetch = (url) => {
+
+  const isMounted = useRef(true)
+
+  const [state, setState] = useState({ data: null, loading: true, error: null })
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    }
+  },[])
+  
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error('Something went wrong')
+        }
+      })
+      .then(data => {
+        if (isMounted.current) {
+          setState({ data: data, loading: false, error: null })
+        }
+      })
+      .catch(() => {
+        setState({
+          data: null,
+          loading: false,
+          error:'No se pudo cargar la info'
+        })
+      })
+  }, [url])
+
+  return state;
+}
+
+export default useFetch
